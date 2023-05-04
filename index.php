bdba32f991398ee36f9d8bea7ff620ded8e1a8cc
ClaudioLeone <claleone92@gmail.com>
2023-05-04 19:12:44 +0200

    Created form

--- index.php
+++ index.php
@@ -12,6 +12,22 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    <h1><?php echo $test_var ?></h1>
+    <h2>Compila e censura</h2>
+    <form action="" method="GET">
+        <div>
+            <label for="text">Testo:</label> <br>
+            <textarea name="text" id="text" cols="30" rows="10"></textarea>
+        </div>
+
+        <div>
+            <label for="badword">Scrivi la parola da censurare: </label>
+            <input type="text" name="badword" id="badword">
+        </div>
+
+        <div>
+            <button class="btn btn-green" type="submit">Censura</button>
+            <button class="btn btn-red" type="reset">Annulla</button>
+        </div>
+    </form>
 </body>
 </html>
